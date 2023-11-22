389c34bc57dcb53acf3e0588a1e197d136bb1bff
chunChu <86117554+DoanTChung-HE176077@users.noreply.github.com>
2023-11-22 22:55:29 +0700

    add provider client

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "react-redux";
+import reducers from "./redux/reducers/";
+import mySage from "./redux/sagas";
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(reducers, applyMiddleware(sagaMiddleware));
@@ -13,7 +14,8 @@ sagaMiddleware.run(mySaga);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  //noi luu tru toan bo du lieu cua ung dung
+  <Provider>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
