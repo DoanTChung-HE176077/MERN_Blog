d0050af28c2cd1084b6c6ab59a7d7fb02e164924
chunChu <86117554+DoanTChung-HE176077@users.noreply.github.com>
2023-11-23 18:31:43 +0700

    done session4
    setting route app.jsx
    - add outlet to navbar.jsx
    - add animation
    - add form login sign out

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.component";
+import UserAuthForm from "./pages/userAuthForm.page";
 
 const App = () => {
   return (
     <>
-      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route path="/signin" element={<UserAuthForm myType="sign-in" />} />
+          <Route path="/signup" element={<UserAuthForm myType="sign-up" />} />
+        </Route>
+      </Routes>
     </>
   );
 };
